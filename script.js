062223220207542b3165423fdd637c119326d9d4
Bejaro <benjrose89@gmail.com>
2022-07-26 20:33:06 +0100

    grid still not filling correctly

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var cellAmt = document.getElementsByClassName("cell");
 
 slider.oninput = function(){
     var gridSize = this.value;
-    counter.innerHTML = ("Grid size: " + gridSize);
+    counter.innerHTML = ("Grid size: " + gridSize + " x " + gridSize);
 }
 
 const generateButton = document.querySelector('.generate');
@@ -14,13 +14,13 @@ const generateButton = document.querySelector('.generate');
 generateButton.addEventListener('click', generateGrid);
 
 function generateGrid(){
-    
     while (container.firstChild && cellAmt.length > 0) {
         container.removeChild(container.firstChild);
     }
+    //container.style.gridTemplateColumns = 'repeat(${gridSize}, 1fr)';
+    //container.style.gridTemplateRows = 'repeat(${gridSize}, 1fr)';
     for (let i = 0; i < slider.value; i++){
         var cell = document.createElement("div");
-        cell.innerHTML = "Cell";
         cell.className = "cell";
         container.appendChild(cell);
     }
