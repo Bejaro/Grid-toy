6c8da98c0204837b54a6b59f7fec44f34e9e620b
Bejaro <benjrose89@gmail.com>
2022-07-28 15:47:54 +0100

    Minimum viable product

--- script.js
+++ script.js
@@ -16,17 +16,27 @@ const generateButton = document.querySelector('.generate');
 generateButton.addEventListener('click', generateGrid);
 
 function generateGrid(){
+    container.classList.remove('hidden');
     let gridSize = slider.value;
+    let totalCells = gridSize * gridSize;
     while (container.firstChild && cellAmt.length > 0) {
         container.removeChild(container.firstChild);
     }
     
-    for (let i = 0; i < slider.value; i++){
+    for (let i = 0; i < totalCells; i++){
         var cell = document.createElement("div");
         cell.className = "cell";
         container.appendChild(cell);
+        
     }
     container.style.gridTemplateColumns = 'repeat('+gridSize+', 1fr)';
     container.style.gridTemplateRows = 'repeat('+gridSize+', 1fr)';
+
+    var cells = container.querySelectorAll('div');
+    cells.forEach(cell => cell.addEventListener('mouseover', activateCell));
+}
+
+function activateCell(){
+    this.classList.add('cell-active');
 }
 
